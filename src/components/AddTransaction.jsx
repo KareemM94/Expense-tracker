9ad21b2006c7aa95668e71ac078b4f8183b2ff8b
KareemM94 <kareem94@hotmail.co.uk>
2023-07-31 14:19:10 +0100

    first commit

---
+++ src/components/AddTransaction.jsx
@@ -0,0 +1,66 @@
+import React, { useState, useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+
+const AddTransaction = () => {
+
+  const { addTransaction } = useContext(GlobalContext);
+
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState(0);
+
+  const handleTextChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleAmountChange = (e) => {
+    setAmount(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    const newTransaction = {
+      id: Math.floor(Math.random() * 100000000),
+      text,
+      amount: +amount
+    }
+
+    addTransaction(newTransaction)
+  }
+
+  
+
+
+
+  return (
+    <>
+      <h3>Add new transaction</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-control">
+          <label htmlFor="text">Text</label>
+          <input
+            type="text"
+            value={text}
+            onChange={handleTextChange}
+            placeholder="Enter text..."
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="amount">
+            Amount <br />
+            (negative - expense, positive - income)
+          </label>
+          <input
+            type="number"
+            value={amount}
+            onChange={handleAmountChange}
+            placeholder="Enter amount..."
+          />
+        </div>
+        <button className="btn">Add transaction</button>
+      </form>
+    </>
+  );
+};
+
+export default AddTransaction;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const header = () => {
+  return (
+    <h2>Expense tracker</h2>
+  )
+}
+
+export default header
